fc471bc75a5506bb1d53d4c65f490e6078862874
Krishi Patel <krishi@gmail.com>
2026-02-16 12:11:26 +0530

    upload installment project

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -4,21 +4,23 @@
 
 <h3>Edit Category</h3>
 
-<form method="POST" action="{{ route('category.update',$category->id) }}">
-@csrf
+<form method="POST" action="{{ route('category.update', $category->id) }}">
+    @csrf
+    @method('PUT')
 
-<div class="mb-2">
-    <label>Category Name</label>
-    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $category->name) }}" required>
-    @error('name')
-        <small class="text-danger d-block mt-1">{{ $message }}</small>
-    @enderror
-</div>
+    <div class="mb-2">
+        <label>Category Name</label>
+        <input type="text" name="name"
+            class="form-control @error('name') is-invalid @enderror"
+            value="{{ old('name', $category->name) }}" required>
 
+        @error('name')
+            <small class="text-danger d-block mt-1">{{ $message }}</small>
+        @enderror
+    </div>
 
-
-<button class="btn btn-success">Update Category</button>
-<a href="{{ route('category.index') }}" class="btn btn-secondary">Back</a>
+    <button class="btn btn-success">Update Category</button>
+    <a href="{{ route('category.index') }}" class="btn btn-secondary">Back</a>
 
 </form>
 
